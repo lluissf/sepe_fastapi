80136bed77b3c5af2b48389de895bd698ed8b0bd
Luís Felipe <luis.felipe.ferreira.da.silva@hotmail.com>
2025-10-07 09:05:11 -0300

    Organizando o main.py

--- backendfast/main.py
+++ backendfast/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return {"message": "Hello World"}
-
+# Simulação de um banco de dados de produtos
 produtos = [
     {"id": 1, "nome": "Notebook", "preco": 3500},
     {"id": 2, "nome": "Mouse", "preco": 80},
@@ -13,10 +9,17 @@ produtos = [
     {"id": 4, "nome": "Monitor", "preco": 1200},
     {"id": 5, "nome": "Impressora", "preco": 300},
 ]
+# Rota raiz
+@app.get("/")
+def hello():
+    return {"message": "Hello World"}
 
+# Listar todos os produtos
 @app.get("/produtos")
 def listar_produtos():
     return produtos
+
+# Obter detalhes de um produto específico
 @app.get("/produtos/{produto_id}")
 def obter_produto(produto_id: int):
     for produto in produtos:

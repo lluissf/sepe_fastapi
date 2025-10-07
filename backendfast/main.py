60ccb8a50c739e247cd238651777dba390cd0875
Luís Felipe <luis.felipe.ferreira.da.silva@hotmail.com>
2025-10-07 09:03:26 -0300

    Retornar uma lista de Produtos

--- backendfast/main.py
+++ backendfast/main.py
@@ -5,3 +5,15 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"message": "Hello World"}
+
+produtos = [
+    {"id": 1, "nome": "Notebook", "preco": 3500},
+    {"id": 2, "nome": "Mouse", "preco": 80},
+    {"id": 3, "nome": "Teclado", "preco": 150},
+    {"id": 4, "nome": "Monitor", "preco": 1200},
+    {"id": 5, "nome": "Impressora", "preco": 300},
+]
+
+@app.get("/produtos")
+def listar_produtos():
+    return produtos

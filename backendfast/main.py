6da9970d0cffc93a3fe1c5f97fb28ed95db906e5
Luís Felipe <luis.felipe.ferreira.da.silva@hotmail.com>
2025-10-07 09:54:15 -0300

    Adicionando o frontend

--- backendfast/main.py
+++ backendfast/main.py
@@ -16,6 +16,8 @@ produtos = [
     {"id": 4, "nome": "Monitor", "preco": 1200},
     {"id": 5, "nome": "Impressora", "preco": 300},
 ]
+
+ultimo_id:int = 5
 # Rota raiz
 @app.get("/")
 def hello():
@@ -27,9 +29,24 @@ def listar_produtos():
     return produtos
 
 # Obter detalhes de um produto específico
-@app.get("/produtos/{produto_id}")
+@app.get("/produtos/{produto_id}", response_model=Produto)
 def obter_produto(produto_id: int):
     for produto in produtos:
         if produto["id"] == produto_id:
             return produto
     return {"error": "Produto não encontrado"}
+
+#Adicionar um novo produto
+#@app.post("/produtos", response_model=Produto)
+#def adicionar_produto(produto: Produto):
+#    produtos.append(produto.dict())
+#    return produto
+
+# Adicionar um novo produto
+@app.post("/produtos/")
+def criar_produto(nome: str, preco: float):
+    global ultimo_id
+    ultimo_id += 1
+    novo_produto = {"id": ultimo_id, "nome": nome, "preco": preco}
+    produtos.append(novo_produto)
+    return novo_produto
